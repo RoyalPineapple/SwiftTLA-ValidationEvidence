e28cc158bd8c1d66d202c6ed46c1abf1c5f20ae6
Alex Odawa <alex@squareup.com>
2026-08-16 12:22:32 +0200

    Fix PlusCal harness checkout path

--- validation/pluscal-oracle-harness/Package.swift
+++ validation/pluscal-oracle-harness/Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "PlusCalOracleHarness",
     platforms: [.macOS(.v14)],
-    dependencies: [.package(path: "../../../SwiftTLA")],
+    dependencies: [.package(path: "../../SwiftTLA")],
     targets: [
         .executableTarget(
             name: "pluscal-oracle-harness",
